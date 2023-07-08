c9b814bbbf1a6f85e6e09c253c9bc7c6748d2d8a
Sanjeevv30 <sanjeevsrivastava107@gmail.com>
2023-07-08 10:23:06 +0530

    fourthCommit

--- demo.js
+++ demo.js
@@ -1,4 +1,4 @@
-console.log("hello");
+//console.log("hello");
 // console.log(document.title);
 // console.log(document.URL);
 // console.log(document.doctype);
@@ -9,17 +9,37 @@ console.log("hello");
 // console.log(document.all[10]);
 // document.all[10].textContent = 'Hello';
 // //GET ELEMENT BY ID
-var headerTitle = document.getElementById('header-title');
-var header = document.getElementById('main-header');
-header.style.borderBottom = 'solid 3px #000';
+// var headerTitle = document.getElementById('header-title');
+// var header = document.getElementById('main-header');
+// header.style.borderBottom = 'solid 3px #000';
 // GET ELEMENT BY CLASS;
 var items = document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[1]);
-items[1].textContent = 'Hello 2';
-items[0].style.fontWeight = 'bold';
-items[1].style.fontWeight = 'bold';
-items[2].style.fontWeight = 'bold';
-items[3].style.fontWeight = 'bold';
-items[2].style.backgroundColor = 'green';
+// console.log(items);
+// console.log(items[1]);
+// items[1].textContent = 'Hello 2';
+// items[0].style.fontWeight = 'bold';
+// items[1].style.fontWeight = 'bold';
+// items[2].style.fontWeight = 'bold';
+// items[3].style.fontWeight = 'bold';
+// items[2].style.backgroundColor = 'green';
+
+// for(let i=0;i<items.length;i++){
+//     items[1].style.backgroundColor = "#f4f4f4";
+// }
+items[4].style.fontWeight = 'bold';
+// GET ELEMENT BY TAGNAME;
+var li = document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+li[1].textContent = 'Hello 2';
+// li[0].style.fontWeight = 'bold';
+// li[1].style.fontWeight = 'bold';
+// li[2].style.fontWeight = 'bold';
+// li[3].style.fontWeight = 'bold';
+// li[4].style.fontWeight = 'bold';
+li[2].style.backgroundColor = 'green';
+
+for(let i=0;i<li.length;i++){
+    li[1].style.backgroundColor = "#f4f4f4";
+}
 
