3b774a10c027780bde6a280df7cf6450079cf4ce
Sanjeevv30 <sanjeevsrivastava107@gmail.com>
2023-07-08 09:59:54 +0530

    third

--- demo.js
+++ demo.js
@@ -17,6 +17,9 @@ var items = document.getElementsByClassName('list-group-item');
 console.log(items);
 console.log(items[1]);
 items[1].textContent = 'Hello 2';
+items[0].style.fontWeight = 'bold';
 items[1].style.fontWeight = 'bold';
-items[1].style.backgroundColor = 'green';
+items[2].style.fontWeight = 'bold';
+items[3].style.fontWeight = 'bold';
+items[2].style.backgroundColor = 'green';
 
